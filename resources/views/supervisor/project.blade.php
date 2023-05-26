2b854063146fc1d092579be5c5ee0b6450cf9809
neroSpace <m.farhan.haykal@gmail.com>
2023-05-26 22:17:23 +0700

    add: something

--- resources/views/supervisor/project.blade.php
+++ resources/views/supervisor/project.blade.php
@@ -11,15 +11,9 @@
             <thead>
                 <tr>
                     <th>No</th>
-<<<<<<< HEAD
                     <th>Category</th>
                     <th>Location</th>
                     <th>Project Name</th>
-=======
-                    <th>Name</th>
-                    <th>Location</th>
-                    <th>Project</th>
->>>>>>> deeeaa2 (fix something)
                     <th>Start Date</th>
                     <th>End Date</th>
                     {{-- <th style="text-align-last: center">Total task</th> --}}
@@ -30,25 +24,14 @@
             <tbody>
                 @foreach ($projects as $p)
                     <tr>
-<<<<<<< HEAD
-                        <td>{{ $p->id }}</td>
-                        <td>{{ $p->cname }}</td>
-                        <td>{{ $p->lname }}</td>
-                        <td>{{ $p->pname }}</td>
-=======
                         <td>{{ $p->cat_id }}</td>
                         <td>{{ $p->cat_name }}</td>
                         <td>{{ $p->loc_name }}</td>
                         <td>{{ $p->proj_name }}</td>
->>>>>>> deeeaa2 (fix something)
                         <td>{{ $p->start_date }}</td>
                         <td>{{ $p->end_date }}</td>
                         <td class="text-center">
-<<<<<<< HEAD
-                            <a href="{{ route('supervisor.project.detail', ['id' => $p->id]) }}">
-=======
-                            <a href="{{ route('supervisor.detail', ['id' => $p->proj_id]) }}">
->>>>>>> deeeaa2 (fix something)
+                            <a href="{{ route('supervisor.project.detail', ['id' => $p->cat_id]) }}">
                                 <button class="btn btn-info font-semibold">Detail</button>
                             </a>
                         </td>
