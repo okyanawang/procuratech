d9fd28dd6eeb5979a6168cc4b16a0973f7900aa2
neroSpace <m.farhan.haykal@gmail.com>
2023-05-27 00:40:02 +0700

    add: status&file attribute into reports table | relation from users to reports table one too many

--- database/migrations/2023_05_26_154536_create_reports_table.php
+++ database/migrations/2023_05_26_154536_create_reports_table.php
@@ -17,9 +17,12 @@ return new class extends Migration
             $table->integer('id', true);
             $table->string('title', 45)->nullable();
             $table->string('description', 45)->nullable();
+            $table->string('status')->nullable();
+            $table->longblob('file')->nullable();
             $table->dateTime('created_at')->nullable();
             $table->dateTime('updated_at')->nullable();
             $table->integer('tasks_id')->index('fk_reports_tasks1_idx');
+            $table->integer('users_id')->index('fk_reports_users1_idx');
         });
     }
 
