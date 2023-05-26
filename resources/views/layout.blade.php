1063418b2193d4ff3610fcfe839af650ddbca0f4
JabalnurIT <jabalnur.it@gmail.com>
2023-05-26 23:07:05 +0700

    add feature: project admin

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -26,13 +26,141 @@
 
     @yield('scripts')
     <script>
-        let table = new DataTable('#myTable', {
-            // options
-        });
+    let table = new DataTable('#myTable', {
+        // options
+    });
 
-        $(document).ready(function() {
-            $('.js-example-basic-single').select2();
-        });
+    $(document).ready(function() {
+        $('.js-example-basic-single').select2();
+    });
+
+    // ADD CATEGORY AND LOCATION INPUTS
+    const addLocationButton = document.getElementById('add-location');
+    const addCategoryButton = document.getElementById('add-category');
+    const locationContainer = document.getElementById('location-container');
+
+    let locationCount = 1;
+    let categoryCount = [
+        [1]
+    ]; // Mengubah nilai awal categoryCount menjadi [[2]]
+
+    locationContainer.addEventListener('click', (e) => {
+        if (e.target.classList.contains('delete-location')) {
+            e.target.parentElement.parentElement.parentElement.remove();
+            const locationContainer = document.getElementById('location-container');
+            if(locationContainer.children[1] == undefined && locationContainer.children[0].children[1].children[0].children[1] != undefined){
+                locationContainer.children[0].children[1].children[0].children[1].remove();
+                // locationContainer.children[0].children[1].children[0].children[0].children[0].lastChild.remove();
+            }
+            
+
+            locationCount--;
+        }
+        if (e.target.classList.contains('delete-category')) {
+
+            const locationContainer = document.getElementById('location-container');
+
+            const currentLocationGroup = e.target.closest('.location-group');
+            const locationIndex = Array.from(locationContainer.children).indexOf(currentLocationGroup);
+            const categoryIndex = Array.from(currentLocationGroup.querySelectorAll('.category-item')).indexOf(e
+                .target.closest('.category-item'));
+
+            e.target.parentElement.remove();
+
+            const currentCategoryGroup = currentLocationGroup.querySelector('.category-group').children[0];
+            
+            if (currentCategoryGroup.children[1] == undefined) {
+                currentCategoryGroup.children[0].children[1].remove();
+            }
+            categoryCount[locationIndex].splice(categoryIndex, 1);
+            
+        }
+    });
+
+    addLocationButton.addEventListener('click', () => {
+        const locationGroup = document.createElement('div');
+
+        locationGroup.className = 'location-group';
+        locationGroup.innerHTML = `
+        <div class="flex flex-col md:flex-row gap-3 mb-2">
+            <div class="grid grid-cols-1 grid-rows-1 gap-2 items-center lg:w-2/3">
+                <label class="mr-3 font-semibold">Location :</label>
+            </div>
+            <div class="grid grid-cols-1 grid-rows-1 gap-2 items-center lg:w-2/3">
+                <label class="mr-3 font-semibold">Category :</label>
+            </div>
+        </div>
+        <div class="flex flex-col md:flex-row gap-3 mb-2">
+            <div class="grid grid-cols-10 grid-rows-1 gap-2 items-center lg:w-2/3">
+                <input name="location[]" type="text" class="input input-bordered w-full col-span-9"
+                    placeholder="location" required />
+                <button type="button" class="delete-location px-2 py-1 text-white bg-red-500 rounded-md"><i
+                    class="fa-solid fa-trash fa-xs"></i></button>
+            </div>
+            <div class="category-group grid grid-cols-1 grid-rows-1 gap-2 items-center lg:w-2/3">
+                    <div class="flex flex-col gap-3">
+                        <div class="category-item grid grid-cols-10 grid-rows-1 gap-2 items-center">
+                            <input name="category[1][]" type="text" class="input input-bordered w-full col-span-9"
+                            placeholder="category" required />
+                        </div>
+                    </div>
+                
+            </div>
+        </div>
+        `;
+        let currentLocationGroup = locationContainer.querySelector('.location-group:last-of-type');
+        const previousLocationDeleteButton = currentLocationGroup.querySelector('.delete-location');
+        // if (previousLocationDeleteButton) {
+        //     previousLocationDeleteButton.parentNode.removeChild(previousLocationDeleteButton);
+        // }
+        locationContainer.appendChild(locationGroup);
+        currentLocationGroup = locationContainer.querySelector('.location-group:last-of-type');
+        if (currentLocationGroup.parentNode.children[1] != undefined && currentLocationGroup.parentNode.children[0]
+            .children[1].children[0].children[1] == undefined) {
+            const buttonDeleteLocation = document.createElement('button');
+            buttonDeleteLocation.className = 'delete-location px-2 py-1 text-white bg-red-500 rounded-md';
+            buttonDeleteLocation.innerHTML = `
+            <i class="fa-solid fa-trash fa-xs"></i>
+            `;
+            const locationItem = locationContainer.children[0].children[1].children[0];
+            locationItem.append(buttonDeleteLocation);
+        }
+
+        locationCount++;
+        categoryCount.push([1]); // Menambahkan [2] ke dalam categoryCount untuk setiap penambahan lokasi
+    });
+
+    addCategoryButton.addEventListener('click', () => {
+        const categoryInput = document.createElement('div');
+        categoryInput.className = 'category-item grid grid-cols-10 grid-rows-1 gap-2 items-center'
+        categoryInput.innerHTML = `
+            <input name="category[${locationCount}][]" type="text" class="input input-bordered w-full col-span-9"
+            placeholder="category" required />
+            <button type="button" class="delete-category px-2 py-1 text-white bg-red-500 rounded-md">
+                <i class="fa-solid fa-trash fa-xs"></i>
+            </button>
+    `;
+
+        const currentLocationGroup = locationContainer.querySelector('.location-group:last-of-type');
+        const currentCategoryGroup = currentLocationGroup.querySelector('.category-group').children[0];
+
+        currentCategoryGroup.appendChild(categoryInput);
+
+        if (currentCategoryGroup.children[1] != undefined && currentCategoryGroup.children[0].children[1] ==
+            undefined) {
+            const buttonDeleteCategory = document.createElement('button');
+            buttonDeleteCategory.className = 'delete-category px-2 py-1 text-white bg-red-500 rounded-md';
+            buttonDeleteCategory.innerHTML = `
+                <i class="fa-solid fa-trash fa-xs"></i>
+            `;
+            currentCategoryGroup.children[0].append(buttonDeleteCategory)
+        }
+        categoryCount[locationCount - 1].push(categoryCount[locationCount - 1].length + 1);
+    });
+
+
+
+    // END OF ADD CATEGORY AND LOCATION INPUTS
     </script>
 
 </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,28 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // gridTemplateRows: (() => {
+      //   const gridRows = {};
+
+      //   for (let i = 1; i <= 100; i++) {
+      //     gridRows[i.toString()] = `repeat(${i}, minmax(0, 1fr))`;
+      //   }
+
+      //   return gridRows;
+      // })(),
+      gridTemplateRows: {
+        '2': 'repeat(2, minmax(0, 1fr))',
+        '3': 'repeat(3, minmax(0, 1fr))',
+        '4': 'repeat(4, minmax(0, 1fr))',
+        '5': 'repeat(5, minmax(0, 1fr))',
+        '6': 'repeat(6, minmax(0, 1fr))',
+        '7': 'repeat(7, minmax(0, 1fr))',
+        '8': 'repeat(8, minmax(0, 1fr))',
+        '9': 'repeat(9, minmax(0, 1fr))',
+        '10': 'repeat(10, minmax(0, 1fr))',
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
