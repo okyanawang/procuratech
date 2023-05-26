370fb58f3c39fad8e31c187a2cfcfe93ccb7b106
fillahaufi <fillahaufi140101@gmail.com>
2023-05-27 02:45:51 +0700

    add: add task and assign staff into it done

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use DB;
+// use Illuminate\Support\Facades\DB;
 
 class TaskController extends Controller
 {
@@ -20,26 +22,69 @@ class TaskController extends Controller
     {
         $validatedData = $request->validate([
             'name' => 'required|unique:tasks|max:255',
-            'description' => 'required',
+            'desc' => 'required',
             'type' => 'required',
-            'status' => 'required',
-            'start_date' => 'required',
-            'end_date' => 'required',
-            'projects_id' => 'required',
+            // 'worker' => 'required',
+            // 'inspector' => 'required',
+            // 'status' => 'required',
+            'startdate' => 'required',
+            'enddate' => 'required',
+            'categories_id' => 'required',
         ]);
+
+        // dd($validatedData);
 
         $task = new Task;
         $task->name = $validatedData['name'];
-        $task->description = $validatedData['description'];
+        $task->description = $validatedData['desc'];
         $task->type = $validatedData['type'];
-        $task->status = $validatedData['status'];
-        $task->start_date = $validatedData['start_date'];
-        $task->end_date = $validatedData['end_date'];
-        $task->projects_id = $validatedData['projects_id'];
+        $task->status = "on progress";
+        $task->start_date = $validatedData['startdate'];
+        $task->end_date = $validatedData['enddate'];
+        $task->categories_id = $validatedData['categories_id'];
         $task->save();
 
-        return redirect()->route('task.index')->with('success', 'Task berhasil ditambahkan');
+        return redirect()->route('supervisor.project.index')->with('success', 'Task berhasil ditambahkan');
     }
+
+    public function assign_staff(Request $request)
+    {
+        $validatedData = $request->validate([
+            'staff' => 'required',
+            // 'inspector' => 'required',
+            'task_id' => 'required',
+        ]);
+
+        // create new users_has_tasks record
+        DB::table('users_has_tasks')->insert([
+            'users_id' => $validatedData['staff'],
+            'tasks_id' => $validatedData['task_id'],
+        ]);
+        // $task->worker = $validatedData['staff'];
+        // $task->save();
+
+        return redirect()->route('supervisor.project.job.detail', $validatedData['task_id'])->with('success', 'Staff berhasil ditambahkan');
+    }
+
+    public function remove_staff(Request $request)
+    {
+        $validatedData = $request->validate([
+            'staff' => 'required',
+            // 'inspector' => 'required',
+            'task_id' => 'required',
+        ]);
+
+        // create new users_has_tasks record
+        DB::table('users_has_tasks')
+            ->where('users_id', $validatedData['staff'])
+            ->where('tasks_id', $validatedData['task_id'])
+            ->delete();
+        // $task->worker = $validatedData['staff'];
+        // $task->save();
+
+        return redirect()->route('supervisor.project.job.detail', $validatedData['task_id'])->with('success', 'Staff berhasil dihapus');
+    }
+
     public function show($id)
     {
         $task = Task::find($id);
