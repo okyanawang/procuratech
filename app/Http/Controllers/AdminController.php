9ffd7166bf43470e4c88327e0f13ec7ec51fe2e6
JabalnurIT <jabalnur.it@gmail.com>
2023-05-27 17:49:51 +0700

    update feature: controller admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\Project;
+use App\Models\Location;
+use App\Models\Category;
+use App\Models\Task;
+use App\Models\Item;
 use Illuminate\Http\Request;
+use DB;
 
 class AdminController extends Controller
 {
@@ -12,11 +17,6 @@ class AdminController extends Controller
     {
         $nuser = User::where('role', '!=', 'Admin IT')->count();
         return view('admin.dashboard')->with('nuser', $nuser);
-    }
-
-    public function project_index()
-    {
-        return view('admin.project');
     }
 
     public function component_index()
@@ -75,15 +75,56 @@ class AdminController extends Controller
         return view('admin.job-detail');
     }
 
-    public function report_index()
+    public function project_index()
     {
-        return view('admin.report');
+        $projects = Project::all();
+        // butuh jumlah lokasi per project
+        // butuh jumlah category per lokasi
+        return view('admin.project.project', ['projects' => $projects]);
     }
 
-    public function report_detail()
+    public function project_detail($id)
     {
-        return view('admin.report-detail');
+        $project = Project::find($id);
+        $locations = Location::where('projects_id', $project->id)->get();
+        $name_pm = User::find($project->id)->name;
+        // butuh jumlah category per lokasi
+        return view('admin.project.project-detail', compact('project', 'locations', 'name_pm'));
+    }
+    
+    public function location_detail($id)
+    {
+        $location = Location::find($id);
+        $project = Project::find($location->projects_id);
+        $name_pm = User::find($project->id)->name;
+        $categories = Category::where('locations_id', $location->id)->get();
+        return view('admin.project.location-detail', compact('project', 'location', 'name_pm', 'categories'));
     }
 
+    public function category_detail($id)
+    {
+        $category = Category::find($id);
+        $location = Location::find($category->locations_id);
+        $project = Project::find($location->projects_id);
+        $name_pm = User::find($project->id)->name;
+        $name_sv = User::find($category->users_id)->name;
+        $tasks = Task::where('categories_id', $category->id)->get();
+        return view('admin.project.category-detail', compact('project', 'location', 'name_pm', 'category', 'name_sv', 'tasks'));
+    }
 
+    public function task_detail($id)
+    {
+        $task = Task::find($id);
+        $category = Category::find($task->categories_id);
+        $location = Location::find($category->locations_id);
+        $project = Project::find($location->projects_id);
+        $name_pm = User::find($project->id)->name;
+        $name_sv = User::find($category->users_id)->name;
+        // butuh list workers
+        $workers = 0;
+        // butuh list items
+        $items = 0;
+        return view('admin.project.task-detail', compact('project', 'location', 'name_pm', 'category', 'name_sv', 'task', 'workers', 'items'));
+    }
+    
 }
