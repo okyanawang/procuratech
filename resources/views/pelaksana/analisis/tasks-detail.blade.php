851aa425ee7abf062c518753a51837cc2279674b
neroSpace <m.farhan.haykal@gmail.com>
2023-05-28 18:29:38 +0700

    enable: staff view page WHOLE

--- resources/views/pelaksana/analisis/tasks-detail.blade.php
+++ resources/views/pelaksana/analisis/tasks-detail.blade.php
@@ -11,36 +11,66 @@
         <div class="my-14">
 
             <div class="flex flex-row">
-                <h1 class="font-bold text-2xl mb-3">Job name</h1>
-                <p class="ml-3 mb-3 self-center"><span>12/12/23</span> - <span>-</span></p>
+                <h1 class="font-bold text-2xl mb-3">{{ $task->name }}</h1>
+                <p class="ml-3 mb-3 self-center"><span style="color: green;">{{ $task->start_date->format('Y-m-d') }}</span> - 
+                    <span style="color: red;">{{ $task->start_date->format('Y-m-d') }}</span></p>
             </div>
             <div class="flex flex-row mb-5">
-                <div class="badge badge-primary mr-1">on progress</div>
-                <div class="badge badge-info mr-1">Analisis</div>
+                <div class="badge badge-primary mr-1">{{ $task->status }}</div>
+                <div class="badge badge-info mr-1">Analyst</div>
             </div>
             <div class="mt-5">
                 <div class="mb-3">
                     <h4 class="font-bold">Job Description</h4>
-                    <p>Hallo</p>
+                    <p>{{ $task->description }}</p>
                 </div>
                 <div class="grid grid-cols-4 gap-1 items-center">
 
                     <div class="mb-3">
                         <h4 class="font-bold">Project Manager</h4>
-                        <p>ikal - <span>0192739012</span></p>
+                        <p>
+                            @foreach ($pm_ass as $pm)
+                                {{ $pm->name }}
+                                <span> - {{ $pm->phone_number }}</span>
+                            @endforeach
+                        </p>
                     </div>
                     <div class="mb-3">
                         <h4 class="font-bold">Supervisor</h4>
-                        <p>ikal - <span>0192739012</span></p>
+                        <p>
+                            @foreach ($spv_ass as $spv)
+                                {{ $spv->name }}
+                                <span> - {{ $spv->phone_number }}</span>
+                            @endforeach
+                        </p>
                     </div>
                     <div class="mb-3">
-                        <h4 class="font-bold">Woker</h4>
-                        <p>ikal <span>as measurer</span> - <span>0192739012</span></p>
+                        <h4 class="font-bold">Teams</h4>
+                        @if ($teams->isNotEmpty())
+                            <p>
+                                @foreach ($teams as $t)
+                                    {{ $t->name }}
+                                    <span> - {{ $t->phone_number }}</span>
+                                    <span> - {{ $t->role }} <br></span>
+                                @endforeach
+                            </p>
+                        @else
+                            <p>No team formed yet</p>
+                        @endif
                     </div>
                     <div class="mb-3">
-                        <h4 class="font-bold">Work Checker</h4>
-                        <p>ikal - <span>0192739012</span></p>
-                    </div>
+                        <h4 class="font-bold">Inspector</h4>
+                        @if ($ins_ass->isNotEmpty())
+                            <p>
+                                @foreach ($ins_ass as $ins)
+                                    {{ $ins->name }}
+                                    <span> - {{ $ins->phone_number }}</span>
+                                @endforeach
+                            </p>
+                        @else
+                            <p>No inspector assigned yet</p>
+                        @endif
+                    </div>                    
                 </div>
 
 
